6e9e4b241c4fc077ac0931e5e559420c31c7dd81
Tim Moore <tcmoore3@gmail.com>
2022-08-09 21:59:02 -0400

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -1,5 +1,5 @@
 from signac_dashboard import Dashboard
-from signac_dashboard.modules import StatepointList, ImageViewer, VideoViewer
+from signac_dashboard.modules import StatepointList, ImageViewer, VideoViewer, TextDisplay
 from signac_dashboard.modules import Notes
 import signac
 
@@ -13,6 +13,14 @@ class PlotDashboard(Dashboard):
         kT = 'kT = {}'.format(job.sp.kT)
         replica = 'rep {}'.format(job.sp.replica)
         return '; '.join((pressure, kT, replica))
+    
+
+def gsd_restart_error_msg(job):
+    err = job.doc.get('restart_gsd_index_error', False)
+    if err:
+        return 'GSD restart error detected.'
+    else:
+        return ''
 
 
 if __name__ == '__main__':
@@ -20,6 +28,7 @@ if __name__ == '__main__':
     modules.append(StatepointList())
     modules.append(ImageViewer(img_globs=['*.png'], name='Plots'))
     modules.append(ImageViewer(img_globs=['gallery/*.png'], name='Aggregate plots', context='ProjectContext'))
+    modules.append(TextDisplay(name='GSD restart error?', message=gsd_restart_error_msg))
     config = {'PER_PAGE': 50}
     pr = signac.get_project('/gpfs/alpine/mat110/proj-shared/patchy-hexagon-equations-of-state')
     PlotDashboard(config=config, modules=modules, project=pr).main()
